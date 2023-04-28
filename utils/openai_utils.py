3d0001c525d5a06ba9eb4954d15e04c96193f17b
jinjie.wu <jinjie.wu@derbysoft.net>
2023-04-28 14:03:15 +0800

    u

--- utils/openai_utils.py
+++ utils/openai_utils.py
@@ -1,32 +1,30 @@
 import os
 import openai
-from . import env
-from .logger_utils import logging
 
-import os
-os.environ["OPENAI_API_KEY"] = env.openai_api_key
-
-openai.api_base = env.openai_api_base
+openai.api_base = os.environ["OPENAI_API_BASE"]
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-class Openai:
-    """OpenAI Connector."""
+from utils.logger_utils import get_logger
+logger = get_logger()
 
+class Openai(object):
     @staticmethod
     def chat_complete(messages, stream=True):
+        """
+        Call OpenAI Chat Completion API with text prompt.
+        """
         kwargs = {
             "model": "gpt-3.5-turbo",
             "messages": messages,
             "timeout": 5,
             "stream": stream,
             "presence_penalty": 1,
+            # "max_tokens": 800,
             "temperature": 0.8
         }
         try:
             response = openai.ChatCompletion.create(**kwargs)
+            # return response["choices"][0]["message"]['content']
             return response
         except Exception as e:
-            logging.error(f"OpenAI API error: {e}")
-            return e
-
-
+            logger.error(f"OpenAI API error: {e}")
